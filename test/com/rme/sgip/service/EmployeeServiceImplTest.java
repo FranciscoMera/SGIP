bc1fb238485cb1a22e2deda89ac551a2b4780cfc
karina <karina@homura>
2017-10-17 17:23:47 -0500

    test servicios empleado

---
+++ test/com/rme/sgip/service/EmployeeServiceImplTest.java
@@ -0,0 +1,63 @@
+package com.rme.sgip.service;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author CesarHL
+ */
+public class EmployeeServiceImplTest {
+
+    public EmployeeServiceImplTest() {
+    }
+
+    /**
+     * Test of findById method, of class EmployeeServiceImpl.
+     */
+    @Test
+    public void testFindById() {
+    }
+
+    /**
+     * Test of saveEmployee method, of class EmployeeServiceImpl.
+     */
+    @Test
+    public void testSaveEmployee() {
+    }
+
+    /**
+     * Test of updateEmployee method, of class EmployeeServiceImpl.
+     */
+    @Test
+    public void testUpdateEmployee() {
+    }
+
+    /**
+     * Test of deleteEmployeeBySsn method, of class EmployeeServiceImpl.
+     */
+    @Test
+    public void testDeleteEmployeeBySsn() {
+    }
+
+    /**
+     * Test of findAllEmployees method, of class EmployeeServiceImpl.
+     */
+    @Test
+    public void testFindAllEmployees() {
+    }
+
+    /**
+     * Test of findEmployeeBySsn method, of class EmployeeServiceImpl.
+     */
+    @Test
+    public void testFindEmployeeBySsn() {
+    }
+
+    /**
+     * Test of isEmployeeSsnUnique method, of class EmployeeServiceImpl.
+     */
+    @Test
+    public void testIsEmployeeSsnUnique() {
+    }
+
+}
