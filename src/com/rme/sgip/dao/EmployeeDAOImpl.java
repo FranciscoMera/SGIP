1b4e66178f777b398581abf7b2d2d8fec8e0827b
karina <karina@homura>
2017-10-17 16:25:31 -0500

    cambios minimos

--- src/com/rme/sgip/dao/EmployeeDAOImpl.java
+++ src/com/rme/sgip/dao/EmployeeDAOImpl.java
@@ -12,30 +12,57 @@ import com.rme.sgip.model.Employee;
 @Repository("employeeDao")
 public class EmployeeDAOImpl extends AbstractDAO<Integer, Employee> implements EmployeeDAO {
 
-	public Employee findById(int id) {
-		return getByKey(id);
-	}
+    /**
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public Employee findById(int id) {
+        return getByKey(id);
+    }
 
-	public void saveEmployee(Employee employee) {
-		persist(employee);
-	}
+    /**
+     *
+     * @param employee
+     */
+    @Override
+    public void saveEmployee(Employee employee) {
+        persist(employee);
+    }
 
-	public void deleteEmployeeBySsn(String ssn) {
-		Query query = getSession().createSQLQuery("delete from Employee where ssn = :ssn");
-		query.setString("ssn", ssn);
-		query.executeUpdate();
-	}
+    /**
+     *
+     * @param ssn
+     */
+    @Override
+    public void deleteEmployeeBySsn(String ssn) {
+        Query query = getSession().createSQLQuery("delete from Employee where ssn = :ssn");
+        query.setString("ssn", ssn);
+        query.executeUpdate();
+    }
 
-	@SuppressWarnings("unchecked")
-	public List<Employee> findAllEmployees() {
-		Criteria criteria = createEntityCriteria();
-		return (List<Employee>) criteria.list();
-	}
+    /**
+     *
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Employee> findAllEmployees() {
+        Criteria criteria = createEntityCriteria();
+        return (List<Employee>) criteria.list();
+    }
 
-	public Employee findEmployeeBySsn(String ssn) {
-		Criteria criteria = createEntityCriteria();
-		criteria.add(Restrictions.eq("ssn", ssn));
-		return (Employee) criteria.uniqueResult();
-	}
+    /**
+     *
+     * @param ssn
+     * @return
+     */
+    @Override
+    public Employee findEmployeeBySsn(String ssn) {
+        Criteria criteria = createEntityCriteria();
+        criteria.add(Restrictions.eq("ssn", ssn));
+        return (Employee) criteria.uniqueResult();
+    }
 
 }
